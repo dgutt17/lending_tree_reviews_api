1b0da386136b0eae51254da9b292fbc6d2d29a06
Dan Gutt <dgutt10@gmail.com>
2022-07-11 16:47:29 -0500

    Creating controllers folder and controllers inside

--- app.rb
+++ app.rb
@@ -1,23 +1,10 @@
-# require_relative 'lib/basic_helpers.rb'
-# require_relative 'lib/models/todo.rb'
-# set :database_file, 'config/database.yml'
+require 'app/controllers/reviews_controller.rb'
+set :database_file, 'config/database.yml'
 
 class App < Sinatra::Base
-  # include BasicHelpers
-  # register Sinatra::ActiveRecordExtension
-  # get '/' do
-  #   'Hello world!'
-  # end
+  use ReviewsController
 
-  # get '/time' do
-  #   get_current_time
-  # end
-
-  # get '/todos' do 
-  #   Todo.all.to_json
-  # end
-
-  # get '/todo/:id' do 
-  #   Todo.find(params[:id]).to_json
-  # end
+  get '/' do 
+    'Home Page'
+  end
 end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,5 @@
+class ReviewsController < ApplicationController
+  get '/fetch_reviews_by_business_url/:url' do 
+    # Todo.find(params[:id]).to_json
+  end
+end
