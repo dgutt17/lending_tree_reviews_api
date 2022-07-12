6432005971452e00131f5346e0e76a252f097071
Dan Gutt <dgutt10@gmail.com>
2022-07-12 10:09:55 -0500

    updating organization

--- lib/brand_id_getter.rb
+++ lib/brand_id_getter.rb
@@ -1,10 +1,12 @@
 require 'open-uri'
 
 class BrandIdGetter
-  attr_reader :url
+  attr_reader :url, :brand_id_css, :brand_id_attribute_name
 
   def initialize(url)
     @url = url
+    @brand_id_css = ENV['BRAND_ID_CSS']
+    @brand_id_attribute_name = ENV['BRAND_ID_ATTRIBUTE_NAME']
   end
 
   def run 
@@ -18,6 +20,8 @@ class BrandIdGetter
   end
 
   def parse_brand_id 
-    document.css(ENV['BRAND_ID_CSS']).first.attributes[ENV['BRAND_ID_ATTRIBUTE_NAME']].value
+    attributes = document.css(brand_id_css).first.attributes
+
+    attributes[brand_id_attribute_name].value
   end
 end
