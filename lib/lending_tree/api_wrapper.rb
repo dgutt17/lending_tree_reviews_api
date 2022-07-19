a8a4c8d291a650ac972640dd37e1e42bb310b10e
Dan Gutt <dgutt10@gmail.com>
2022-07-19 09:25:07 -0500

    Using an env variable for page size param for api call url

--- lib/lending_tree/api_wrapper.rb
+++ lib/lending_tree/api_wrapper.rb
@@ -17,7 +17,7 @@ module LendingTree
     private
 
     def create_url(brand_id)
-      "https://www.lendingtree.com/wp-json/review/proxy?RequestType=&productType=&brandId=#{brand_id}&requestmode=reviews,stats,ratingconfig,propertyconfig&page=0&sortby=reviewsubmitted&sortorder=desc&pagesize=1000"
+      "https://www.lendingtree.com/wp-json/review/proxy?RequestType=&productType=&brandId=#{brand_id}&requestmode=reviews,stats,ratingconfig,propertyconfig&page=0&sortby=reviewsubmitted&sortorder=desc&pagesize=#{ENV['PAGE_SIZE']}"
     end
 
     def nonce
