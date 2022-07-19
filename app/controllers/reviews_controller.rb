5b76abec7a4732a4dd81840be78b5cf035bccfec
Dan Gutt <dgutt10@gmail.com>
2022-07-19 08:55:18 -0500

    Created a URLHelper for reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  include URLHelper
+
   set :raise_errors, false
   set :show_exceptions, false
 
@@ -17,38 +19,6 @@ class ReviewsController < ApplicationController
   end
 
   private 
-
-  def valid_url_param  
-    url = params['url']
-    if !url.include?('lendingtree.com/reviews/business')
-      @error_message = 'Incorrect Base url in url param'
-      return false
-    end
-
-    name_and_id = url.split('/reviews/business/').last
-    name = name_and_id.first
-    id = name_and_id.last
-
-    if name !~ /^[a-zA-Z0-9\-]+$/
-      @error_message = 'Incorrectly formatted business name'
-      return false
-    end
-
-    if id !~ /\A\d+\Z/
-      @error_message = 'Business Id not present or not an integer'
-      return false
-    end
-
-    true
-  end
-
-  def set_lending_tree_id_and_business_name
-    name_and_id = params['url'].split('/reviews/business/').last
-    name_and_id = name_and_id.split('/')
-
-    @lending_tree_id = name_and_id.last
-    @business_name = name_and_id.first
-  end
 
   def set_brand_id
     if business.nil?
---
+++ app/helpers/url_helper.rb
@@ -0,0 +1,33 @@
+module URLHelper
+  def valid_url_param  
+    url = params['url']
+    if !url.include?('lendingtree.com/reviews/business')
+      @error_message = 'Incorrect Base url in url param'
+      return false
+    end
+
+    name_and_id = url.split('/reviews/business/').last
+    name = name_and_id.first
+    id = name_and_id.last
+
+    if name !~ /^[a-zA-Z0-9\-]+$/
+      @error_message = 'Incorrectly formatted business name'
+      return false
+    end
+
+    if id !~ /\A\d+\Z/
+      @error_message = 'Business Id not present or not an integer'
+      return false
+    end
+
+    true
+  end
+
+  def set_lending_tree_id_and_business_name
+    name_and_id = params['url'].split('/reviews/business/').last
+    name_and_id = name_and_id.split('/')
+
+    @lending_tree_id = name_and_id.last
+    @business_name = name_and_id.first
+  end
+end
