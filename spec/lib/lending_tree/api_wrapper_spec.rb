ba6830b1d511c5d1400671a5087be66a33859e8d
Dan Gutt <dgutt10@gmail.com>
2022-07-20 10:59:34 -0500

    Fixed failing spec

--- spec/lib/lending_tree/api_wrapper_spec.rb
+++ spec/lib/lending_tree/api_wrapper_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 RSpec.describe LendingTree::ApiWrapper, type: :lib do
   let(:response_json) { File.read('spec/fixtures/api_response.json') }
+  let(:response) {OpenStruct.new(code: 200, body: response_json)}
 
   it 'should return a json string' do 
     api_wrapper = LendingTree::ApiWrapper.new('123456abc')
     allow(api_wrapper).to receive(:nonce) { '57rytyf8' }
-    allow(api_wrapper).to receive(:http_request) { response_json }
+    allow(api_wrapper).to receive(:http_request) { response }
 
-    expect(api_wrapper.call.class).to eq(String)
+    test_response = api_wrapper.call
+    expect(test_response.body.class).to eq(String)
   end
 
   it 'has the correct value for the NONCE constant' do 
